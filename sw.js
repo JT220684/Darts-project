d34b8a6314e1b7c679cfe8c72897d1092ac58111
JT220684 <taylor.jamie1108@gmail.com>
2026-05-14 21:02:01 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const C='darts-v1';
+const F=['./index.html','./manifest.json','./sw.js'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(F)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
